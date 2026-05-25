74506fbbc748facce9f643de4ee4c91f527462c4
Zoabi User <user@zoabi.app>
2026-05-25 14:53:48 -0700

    chore: commit latest changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ik9blo9l1dk"
+    "revision": "0.gvq1ji48amo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
